9a407a59e640949d9ea04e54ce128c79601e288e
Jeremy Roach <jrmyrch@yahoo.com>
2009-03-16 21:47:42 -0500

    unfinished debugger.diff

---
+++ yield_debugger.rb
@@ -0,0 +1,49 @@
+
+module YieldDebugger
+
+  def self.all_methods(obj)
+    case obj
+    when CompiledMethod
+      cmethods = [obj]
+      obj.literals.each do |o|
+        cmethods += all_methods(o)
+      end
+      cmethods
+    else
+      []
+    end
+  end
+
+  def self.modify_instructions(ic)
+
+    if ic.iseq[0] == :yield_debugger
+      return
+    end
+
+    ic.insert(0, [:yield_debugger])
+
+    i = ic.next(1)
+    while i
+      if ic.next(i)
+        ic.insert(ic.next(i), [:noop])
+      else
+        ic.insert(-1, [:noop])
+      end
+      ic.swap(i)
+      i = ic.next(i.succ)
+    end
+
+    ic.finalize
+  end
+
+  def self.run(cm_main)
+
+    for cm in all_methods(cm_main)
+      ic = InstructionChanges.new(cm)
+      modify_instructions(ic)
+    end
+
+    cm_main
+  end
+end
+
